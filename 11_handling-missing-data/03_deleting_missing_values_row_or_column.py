28325aef062722e9002749120005db28478f1a02
shivam <shvam12340987@gmail.com>
2025-10-15 10:16:50 +0530

    Update 03_deleting_missing_values_row_or_column.py

--- 11_handling-missing-data/03_deleting_missing_values_row_or_column.py
+++ 11_handling-missing-data/03_deleting_missing_values_row_or_column.py
@@ -10,4 +10,9 @@ df = pd.DataFrame(data)
 
 dataAfterRemoveNullValues = df.dropna(axis=0)                # axis=0 remove complete row if the null exist 
                                                              # axis=1 remove complete column in if in column any value is null
+
+# or we can drop row from specific columns 
+removedNullForSpecificColumns=df.dropna(subset=['salary','company'])   # this will delete rows if the null value exist in these columns
+
+                                                             
 print(dataAfterRemoveNullValues)
